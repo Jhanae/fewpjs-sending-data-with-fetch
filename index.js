bd1f96fd4c09968f2cfe308692cc3dea261b2199
Jhanae <jhanaechisholm12@gmail.com>
2021-05-23 18:45:03 -0400

    commit tree 3963802b21406d3d8be04a48bdd759ab1dfd8fd7

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+   fetch('http://localhost:3000/users', configObj)
+            .then(function(response) {
+                return response.json('body')})
+                .then(function(e){
+                    document.body.innerHTML += `<div>\n
+                    <p>${e.name}</p>\n
+                    <p>${e.email}</p>\n
+                    </div>`
+                })
+                 .catch(function(error){
+                     document.body.innerHTML += document.querySelector('message')
+                     console.log(error)
+                 })
+             
+            
+}
+submitData(name,email)
